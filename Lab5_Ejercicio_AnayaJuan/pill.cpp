cee496c439e5398ab81d436055cff33365cd8970
JuanAnaya0730 <juan.anayar@udea.edu.co>
2022-09-01 02:25:55 -0500

    ahora la pildora se ubica en la posicion que se le pasa, tambien se implementaron los metodos que definen su estructura

--- Lab5_Ejercicio_AnayaJuan/pill.cpp
+++ Lab5_Ejercicio_AnayaJuan/pill.cpp
@@ -1,6 +1,18 @@
 #include "pill.h"
 
-Pill::Pill()
+Pill::Pill(const int &x, const int &y)
 {
+    this->setPos(x, y);
+}
 
+QRectF Pill::boundingRect() const
+{
+    return QRectF(-0.5, -0.5, WH_PILL, WH_PILL);
+}
+
+void Pill::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
+{
+    painter->drawRect(0, 0, WH_PILL-1, WH_PILL-1);
+    Q_UNUSED(option);
+    Q_UNUSED(widget);
 }
