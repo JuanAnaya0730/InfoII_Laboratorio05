e46e0d0b38fffed7ca469d19464098bc282eabf7
JuanAnaya0730 <juan.anayar@udea.edu.co>
2022-09-01 01:21:18 -0500

    se crea una instancia de la clase que muestra el juego

--- Lab5_Ejercicio_AnayaJuan/main.cpp
+++ Lab5_Ejercicio_AnayaJuan/main.cpp
@@ -1,8 +1,13 @@
+#include "gamewindow.h"
+
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    GameWindow gw;
+    gw.show();
 
     return a.exec();
 }
