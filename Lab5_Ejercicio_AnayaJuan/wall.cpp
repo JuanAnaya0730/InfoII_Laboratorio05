e829cbde02497b1f777ac89bea1c50ee3ac39cb2
JuanAnaya0730 <juan.anayar@udea.edu.co>
2022-09-01 02:06:40 -0500

    ahora el muros se posiciona en las coordenadas que se le pasan, tambien se implementaron las funciones que le dan la estructura al mismo

--- Lab5_Ejercicio_AnayaJuan/wall.cpp
+++ Lab5_Ejercicio_AnayaJuan/wall.cpp
@@ -1,6 +1,18 @@
 #include "wall.h"
 
-Wall::Wall()
+Wall::Wall(const int &x, const int &y)
 {
+    this->setPos(x, y);
+}
 
+QRectF Wall::boundingRect() const
+{
+    return QRectF(-0.5, -0.5, WH_WALL, WH_WALL);
+}
+
+void Wall::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
+{
+    painter->drawRect(0, 0, WH_WALL-1, WH_WALL-1);
+    Q_UNUSED(option);
+    Q_UNUSED(widget);
 }
