35921c072d93b7404f1256a7eedff3ff04b33397
JuanAnaya0730 <juan.anayar@udea.edu.co>
2022-09-01 02:20:43 -0500

    se definio en ancho y alto de las pildoras, tambien se crearon los prototipos de las funciones que definiran su estructura

--- Lab5_Ejercicio_AnayaJuan/pill.h
+++ Lab5_Ejercicio_AnayaJuan/pill.h
@@ -2,9 +2,16 @@
 #define PILL_H
 
 #include <QGraphicsItem>
+#include <QPainter>
+
+#define WH_PILL 2 // Ancho y alto de la pildora
 
 class Pill : public QGraphicsItem
 {
+private:
+    QRectF boundingRect() const;
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget = nullptr);
+
 public:
     Pill();
 };
