940b2008f19920c3ad18473507e94837b1475dd2
JuanAnaya0730 <juan.anayar@udea.edu.co>
2022-09-01 23:28:19 -0500

    se implento la funcion que lee el mapa, de momento solo procesa los muros. Tambien se añadieron los muros a la escena

--- Lab5_Ejercicio_AnayaJuan/level.cpp
+++ Lab5_Ejercicio_AnayaJuan/level.cpp
@@ -1,6 +1,34 @@
 #include "level.h"
+#include <fstream>
 
-Level::Level(QObject *parent) :
+#include <QDebug>
+
+Level::Level(const QString &fileName, QObject *parent) :
     QGraphicsScene(parent)
 {
+    this->setSceneRect(0, 0, WH_WALL*29, WH_WALL*32);
+    this->loadLevel(fileName);
+
+    for(int i=0; i < maze.size(); ++i){ this->addItem(maze[i]); }
+}
+
+void Level::loadLevel(const QString &fileName)
+{
+    std::ifstream file;
+    std::string line;
+    QList<std::string> textMap;
+
+    file.open("../Lab5_Ejercicio_AnayaJuan/levels/" + fileName.toStdString());
+    if(!file.is_open()){ qDebug() << "efectivamente mi compai"; }
+    while(!file.eof()){
+        std::getline(file, line);
+        textMap.append(line);
+    }
+    file.close();
+
+    for(int i=0; i < textMap.size(); ++i){
+        for(int j=0; j < (int)textMap[i].length(); ++j){
+            if(textMap[i][j] == 'X'){this->maze.append(new Wall(j*WH_WALL, i*WH_WALL)); }
+        }
+    }
 }
