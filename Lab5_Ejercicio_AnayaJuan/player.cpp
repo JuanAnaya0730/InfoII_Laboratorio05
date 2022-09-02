4eff96e506b2f27dd2fded40437ab058b2a2bef1
JuanAnaya0730 <juan.anayar@udea.edu.co>
2022-09-02 01:04:25 -0500

    se implento el metodo mover, actualizar posicion y el que asigna una nueva direccion

--- Lab5_Ejercicio_AnayaJuan/player.cpp
+++ Lab5_Ejercicio_AnayaJuan/player.cpp
@@ -1,13 +1,33 @@
 #include "player.h"
 
-Player::Player(const int &x, const int &y, QObject *parent)
-    : QObject{parent}, posX(x), posY(y)
+Player::Player(const int &x, const int &y, Direction d, QObject *parent)
+    : QObject{parent}, posX(x), posY(y), direction(d)
 {
     container = new QGraphicsRectItem(0, 0, WH_PLAYER-1, WH_PLAYER-1);
+    container->setPos(posX, posY);
+}
+
+void Player::updatePos()
+{
     container->setPos(posX, posY);
 }
 
 QGraphicsRectItem *Player::getContainer() const
 {
     return container;
+}
+
+void Player::setDirection(Direction newDirection)
+{
+    direction = newDirection;
+}
+
+void Player::move()
+{
+    if(direction == Direction::Up){ posY -= VEL; }
+    else if(direction == Direction::Down){ posY += VEL; }
+    else if(direction == Direction::Left){ posX -= VEL; }
+    else if(direction == Direction::Right){ posX += VEL; }
+
+    this->updatePos();
 }
