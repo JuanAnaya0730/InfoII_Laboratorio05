d3800f207c95378a277e35c47d0b88f5d09222ef
JuanAnaya0730 <juan.anayar@udea.edu.co>
2022-09-02 00:00:46 -0500

    ahora se inicializa el contenedor y se implento el metodo que lo retorna

--- Lab5_Ejercicio_AnayaJuan/player.cpp
+++ Lab5_Ejercicio_AnayaJuan/player.cpp
@@ -3,5 +3,10 @@
 Player::Player(QObject *parent)
     : QObject{parent}
 {
+    container = new QGraphicsRectItem(0, 0, WH_PLAYER-1, WH_PLAYER-1);
+}
 
+QGraphicsRectItem *Player::getContainer() const
+{
+    return container;
 }
