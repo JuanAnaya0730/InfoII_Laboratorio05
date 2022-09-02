5f8d1e891107c2eb2bb27be10145b96e09b7b51b
JuanAnaya0730 <juan.anayar@udea.edu.co>
2022-09-01 23:53:13 -0500

    se añadio un nuevo atributo, este es el el cuadrado que contendra al personaje y se usara para las colisiones con los muros, tambien se definio una constante para el ancho y alto del atributo

--- Lab5_Ejercicio_AnayaJuan/player.h
+++ Lab5_Ejercicio_AnayaJuan/player.h
@@ -2,10 +2,16 @@
 #define PLAYER_H
 
 #include <QObject>
+#include <QGraphicsRectItem>
+
+#define WH_PLAYER 16 // Ancho y alto para el contorno del personaje
 
 class Player : public QObject
 {
     Q_OBJECT
+private:
+    QGraphicsRectItem *container;
+
 public:
     explicit Player(QObject *parent = nullptr);
 
