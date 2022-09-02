52b016a547d2063d92fafdc5067982464010b1df
JuanAnaya0730 <juan.anayar@udea.edu.co>
2022-09-02 02:09:56 -0500

    se especifico que se debe hacer cuando se presiona una de las teclas validas

--- Lab5_Ejercicio_AnayaJuan/gamewindow.cpp
+++ Lab5_Ejercicio_AnayaJuan/gamewindow.cpp
@@ -10,12 +10,16 @@ void GameWindow::keyPressEvent(QKeyEvent *event)
 {
     switch(event->key()){
     case Qt::Key_Up:
+        levelOne->setPlayerDirection(Direction::Up);
         break;
     case Qt::Key_Down:
+        levelOne->setPlayerDirection(Direction::Down);
         break;
     case Qt::Key_Left:
+        levelOne->setPlayerDirection(Direction::Left);
         break;
     case Qt::Key_Right:
+        levelOne->setPlayerDirection(Direction::Right);
         break;
     }
 }
